944583c8acb5a4f20e72b48fa129d3301339d58a
RenanMarozzi <72401989+RenanMarozzi@users.noreply.github.com>
2021-05-05 15:42:03 -0300

    Update AppVeiculo.java

--- AppVeiculo.java
+++ AppVeiculo.java
@@ -1,13 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Renan
- */
 public class AppVeiculo {
     
+    public static void main(String[] args) {
+
+        Veiculo[] veiculo = new Veiculo[4];
+        veiculo[0] = new Veiculo("Grand Cherokee", "Jeep", 2019, 280000, "ABC-1234", "flex");
+        veiculo[1] = new Veiculo("Compass", "Jeep", 2020, 120000, "DEF-4321", "gasolina");
+        veiculo[2] = new Veiculo("Fusca", "Volkswagem", 1971, 12000, "LOL-9941", "gasolina");
+        veiculo[3] = new Veiculo("Uno", "Fiat", 1996, 8000, "JKL-0018", "gasolina");
+        
+        veiculo[0].imprimir();
+        veiculo[1].imprimir();
+        veiculo[2].imprimir();
+        veiculo[3].imprimir();
+
+ }
 }
